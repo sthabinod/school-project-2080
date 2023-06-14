0555dca51032f5f49c31a03b08fff3a70ea9675a
incognito <stha.binod1000@gmail.com>
2023-06-14 19:13:32 +0545

    CHange

--- config/settings/base.py
+++ config/settings/base.py
@@ -146,7 +146,7 @@ MIDDLEWARE = [
 # STATIC
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(BASE_DIR / "staticfiles_build")
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
