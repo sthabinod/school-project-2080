ebd39335da53a3fd6bb2c02753b25ef4c623b641
incognito <stha.binod1000@gmail.com>
2023-06-14 19:12:00 +0545

    CHange

--- build_files.sh
+++ build_files.sh
@@ -1,25 +1,4 @@
-
-{
-    "version":2,
-    "builds": [{
-        "src": "config/wsgi.py",
-        "use": "@vercel/python",
-        "config": { "maxLambdaSize": "15mb","runtime":"python:3.10" }
-    },
-    {
-        "src": "build_files.sh",
-        "use": "@vercel/static-build",
-        "config": { "distDir": "statifiles_build" }
-    }
-    ],
-    "routes": [
-        {
-            "src": "/static/(.*)",
-            "dest": "/static/$1"
-        },
-        {
-            "src": "/(.*)",
-            "dest": "config/wsgi.py"
-        }
-    ]
-}
+echo "BUILD START";
+python3.10 -m pip install -r requirements/local.txt;
+python3.10 manage.py collectstatic --noinput --clear;
+echo "BUILD END";
